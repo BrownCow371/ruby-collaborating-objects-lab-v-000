b45ad1583137dfe23e5e86bb55a47315560ff7cd
Jamie Brown <jmbrowncow@gmail.com>
2018-07-10 13:04:05 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,7 +14,8 @@ attr_accessor :path
   end
 
   def files_alternative
-    @files ||= Dir.glob("#{path}/*.mp3").collect do |f| f.gsub("#{path}/", "")
+    @files ||= Dir.glob("#{path}/*.mp3").collect do 
+    |f| f.gsub("#{path}/", "")
     binding.pry
   end
   end
